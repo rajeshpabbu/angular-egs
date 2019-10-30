9c3b4a7364705ca37bfb8a5ab7d5117c057df204
Rajesh Pabbu <rajeshpabbu548@gmail.com>
2019-10-30 10:59:13 -0400

    loop through object examples

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  loop = {
+    a: 1,
+    b: 2,
+    c: 3,
+    r: 4,
+    d: 5,
+    e: 6,
+    f: 7
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
+  code = `   //type script code
+  var c = 123;
+  var r = 2;
+  function();
+  `;
 }
